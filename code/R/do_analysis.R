22a780235831dc580b9c45432e352f74a589a7d4
Rajabalizadeh <82872991+Rajabalizadeh@users.noreply.github.com>
2021-05-02 13:13:53 +0430

    Update do_analysis.R

--- code/R/do_analysis.R
+++ code/R/do_analysis.R
@@ -34,7 +34,6 @@ tab_regression <-  prepare_regression_table(
     c("ln_gdp_capita", "unemployment"),
     c("ln_gdp_capita", "unemployment")
   ),
-  feffects = list("", "", "year","country"),
   reffects = list("", "", "year","country"),
   cluster = list("", "",  "year","country"),
   format = "latex"
